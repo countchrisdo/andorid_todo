f4e06e48671503635ea1205ab620ccd6e216d6e9
countchrisdo <crburwell@yahoo.com>
2022-02-28 14:15:52 -0500

    Too inti 2 quit

---
+++ app/src/main/java/com/example/todolist/TodoAdapter.kt
@@ -0,0 +1,12 @@
+package com.example.todolist
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class TodoAdapter(
+    private val todos: MutableList<Todo>
+) {
+    class TodoViewHolder(itemView: View) : RecyclerView
+}
+
+//test comment
